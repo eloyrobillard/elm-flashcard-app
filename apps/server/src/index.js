5e84918423f67b151016564b7308187289a0cb00
Eloy Robillard <eloy.robillard@gmail.com>
2025-08-09 10:58:24 +0900

    First commit

---
+++ apps/server/src/index.js
@@ -0,0 +1,25 @@
+import fs from "node:fs";
+import http from "node:http";
+import util from "node:util";
+
+const options = {
+  filename: { type: "string", default: "deck" },
+  filepath: { type: "string", default: process.cwd() },
+};
+
+const {
+  values: { filename, filepath },
+} = util.parseArgs({ args: process.argv.slice(2), options });
+
+const server = http.createServer();
+
+server.on("request", (_req, res) => {
+  const path = `${filepath}/${filename}`;
+
+  fs.readFile(path, (err, data) => {
+    if (err) res.end(err.message);
+    else res.end(data);
+  });
+});
+
+server.listen(8001, "127.0.0.1", () => console.log("Listening on port 8001"));
