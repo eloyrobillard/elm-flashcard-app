eb862a230096406a5983bf8dc6c3bdc1c49e77d3
Eloy Robillard <eloy.robillard@gmail.com>
2025-08-21 14:28:47 +0900

    Add tests for saveDeck and readDeck

--- apps/server/src/tests/repositories.test.js
+++ apps/server/src/tests/repositories.test.js
@@ -1,24 +1,67 @@
+import fs from "node:fs";
 import fsp from "node:fs/promises";
 
 import * as repositories from "../repositories.js";
 
-test("make an exact copy of deck", async () => {
+describe("repositories", () => {
   const src = "./test";
-  await fsp.writeFile(src, "Test contents.");
+  const dest = "./dest";
 
-  const dest = "./test.dest";
+  beforeEach(() => fs.writeFileSync(src, ""));
 
-  const { status, message } = await repositories.backupDeck(src, dest);
+  afterEach(() => fs.rmSync(src));
 
-  expect(status).not.toBeNull();
-  expect(status).toBeDefined();
-  expect(message).not.toBeNull();
-  expect(message).toBeDefined();
+  it("should create a deck", async () => {
+    const { status, message } = await repositories.saveDeck(dest, "Test body");
 
-  if (status === "ok") {
+    expect(status).not.toBeNull();
+    expect(status).toBeDefined();
+    expect(message).not.toBeNull();
+    expect(message).toBeDefined();
+
+    if (status === "ok") {
+      try {
+        const body = await fsp.readFile(dest);
+        expect(body).toBe("Test contents");
+        fs.rmSync(dest);
+      } catch (_) {}
+    }
+  });
+
+  it("should read the contents of a deck", async () => {
+    const data = "test message";
+
     try {
-      const cpContents = await fsp.readFile(dest);
-      expect(cpContents).toBe("Test contents");
+      await fsp.writeFile(src, data);
     } catch (_) {}
-  }
+
+    const res = await repositories.readDeck(src);
+
+    expect(res.status).not.toBeNull();
+    expect(res.status).toBeDefined();
+
+    if (res.status === "ok") {
+      expect(res.data).toBe(data);
+    } else {
+      expect(res.message).not.toBeNull();
+      expect(res.message).toBeDefined();
+    }
+  });
+
+  it("should make an exact copy of deck", async () => {
+    const { status, message } = await repositories.backupDeck(src, dest);
+
+    expect(status).not.toBeNull();
+    expect(status).toBeDefined();
+    expect(message).not.toBeNull();
+    expect(message).toBeDefined();
+
+    if (status === "ok") {
+      try {
+        const cpContents = await fsp.readFile(dest);
+        expect(cpContents).toBe("Test contents");
+        fs.rmSync(dest);
+      } catch (_) {}
+    }
+  });
 });
