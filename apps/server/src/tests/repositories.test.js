0a516097863f39364cecb52ac36b2c8b7f01285f
Eloy Robillard <eloy.robillard@gmail.com>
2025-08-22 09:02:08 +0900

    Restructure tests

--- apps/server/src/tests/repositories.test.js
+++ apps/server/src/tests/repositories.test.js
@@ -11,57 +11,66 @@ describe("repositories", () => {
 
   afterEach(() => fs.rmSync(src));
 
-  it("should create a deck", async () => {
-    const { status, message } = await repositories.saveDeck(dest, "Test body");
+  describe("saveDeck", () => {
+    it("should create a deck", async () => {
+      const { status, message } = await repositories.saveDeck(
+        dest,
+        "Test body",
+      );
 
-    expect(status).not.toBeNull();
-    expect(status).toBeDefined();
-    expect(message).not.toBeNull();
-    expect(message).toBeDefined();
+      expect(status).not.toBeNull();
+      expect(status).toBeDefined();
+      expect(message).not.toBeNull();
+      expect(message).toBeDefined();
 
-    if (status === "ok") {
-      try {
-        const body = await fsp.readFile(dest);
-        expect(body).toBe("Test contents");
-        fs.rmSync(dest);
-      } catch (_) {}
-    }
+      if (status === "ok") {
+        try {
+          const body = await fsp.readFile(dest);
+          expect(body).toBe("Test contents");
+          fs.rmSync(dest);
+        } catch (_) {}
+      }
+    });
   });
 
-  it("should read the contents of a deck", async () => {
-    const data = "test message";
+  describe("readDeck", () => {
+    it("should read the contents of a deck", async () => {
+      const data = "test message";
 
-    try {
-      await fsp.writeFile(src, data);
-    } catch (_) {}
+      try {
+        await fsp.writeFile(src, data);
+      } catch (_) {}
 
-    const res = await repositories.readDeck(src);
+      const res = await repositories.readDeck(src);
 
-    expect(res.status).not.toBeNull();
-    expect(res.status).toBeDefined();
+      expect(res.status).not.toBeNull();
+      expect(res.status).toBeDefined();
 
-    if (res.status === "ok") {
-      expect(res.data).toBe(data);
-    } else {
-      expect(res.message).not.toBeNull();
-      expect(res.message).toBeDefined();
-    }
+      if (res.status === "ok") {
+        expect(res.data).toBe(data);
+      } else {
+        expect(res.message).not.toBeNull();
+        expect(res.message).toBeDefined();
+      }
+    });
   });
 
-  it("should make an exact copy of deck", async () => {
-    const { status, message } = await repositories.backupDeck(src, dest);
+  describe("backupDeck", () => {
+    it("should make an exact copy of deck", async () => {
+      const { status, message } = await repositories.backupDeck(src, dest);
 
-    expect(status).not.toBeNull();
-    expect(status).toBeDefined();
-    expect(message).not.toBeNull();
-    expect(message).toBeDefined();
+      expect(status).not.toBeNull();
+      expect(status).toBeDefined();
+      expect(message).not.toBeNull();
+      expect(message).toBeDefined();
 
-    if (status === "ok") {
-      try {
-        const cpContents = await fsp.readFile(dest);
-        expect(cpContents).toBe("Test contents");
-        fs.rmSync(dest);
-      } catch (_) {}
-    }
+      if (status === "ok") {
+        try {
+          const cpContents = await fsp.readFile(dest);
+          expect(cpContents).toBe("Test contents");
+          fs.rmSync(dest);
+        } catch (_) {}
+      }
+    });
   });
 });
