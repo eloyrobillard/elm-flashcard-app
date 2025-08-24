381ca4d112d99ceb0961e10d2c7323f21478a086
Eloy Robillard <eloy.robillard@gmail.com>
2025-08-24 18:59:57 +0900

    Translate exceptions to errors

--- apps/server/src/errors.js
+++ apps/server/src/errors.js
@@ -1,3 +1,12 @@
-export const invalidBody = "invalidBody";
+export const typeError = "typeError";
+export const invalidFormat = "invalidFormat";
 export const invalidPath = "invalidPath";
 export const unknownError = "unknownError";
+
+export const translateError = (e) => {
+  if (!e.name || !e.message) return unknownError;
+
+  if (e.name === "Error" && e.message.startsWith("ENOENT")) return invalidPath;
+  else if (e.name === "TypeError") return typeError;
+  else return unknownError;
+};
--- apps/server/src/repositories.js
+++ apps/server/src/repositories.js
@@ -7,11 +7,11 @@ export const readDeck = async (path) => {
   try {
     const deck = await fsp.readFile(path);
     return { status: "ok", data: deck.toString() };
-  } catch (err) {
+  } catch (e) {
     return {
       status: "error",
-      error: errors.deckReadFailure,
-      message: "Could not read deck: " + err.message,
+      error: errors.translateError(e),
+      message: "Could not read deck: " + e.message,
     };
   }
 };
@@ -24,11 +24,11 @@ export const backupDeck = async (src, dest) => {
   try {
     await fsp.copyFile(src, dest);
     return { status: "ok", message: "Backed up current deck to: " + dest };
-  } catch (err) {
+  } catch (e) {
     return {
       status: "error",
-      error: errors.deckBackupFailure,
-      message: "Failed to back up current deck: " + err.message,
+      error: errors.translateError(e),
+      message: "Failed to back up current deck: " + e.message,
     };
   }
 };
@@ -43,13 +43,9 @@ export const saveDeck = async (path, body) => {
     await fsp.writeFile(path, body);
     return { status: "ok", message: "Saved new deck to: " + path };
   } catch (e) {
-    let error = errors.unknownError;
-    if (e.name === "Error" && e.message.startsWith("ENOENT"))
-      error = errors.invalidPath;
-
     return {
       status: "error",
-      error,
+      error: errors.translateError(e),
       message: "Failed to save new deck: " + e.message,
     };
   }
