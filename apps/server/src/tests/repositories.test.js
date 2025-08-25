efc813a8384251612a967588a13ab4427278549f
Eloy Robillard <eloy.robillard@gmail.com>
2025-08-25 11:03:33 +0900

    Remove errors from repositories tests

--- apps/server/src/tests/repositories.test.js
+++ apps/server/src/tests/repositories.test.js
@@ -13,23 +13,23 @@ describe("repositories", () => {
 
   describe("saveDeck", () => {
     it("should create a deck", async () => {
-      const { status, message } = await repositories.saveDeck(
-        dest,
-        "Test body",
+      const { tag } = await repositories.saveDeck(dest, "Test body");
+
+      expect(tag).toBe("ok");
+
+      const body = await fsp.readFile(dest, { encoding: "utf8" });
+      expect(body).toBe("Test body");
+
+      fs.rmSync(dest);
+    });
+
+    it("should fail to create a deck when the path is invalid", async () => {
+      const { tag } = await repositories.saveDeck(
+        "./non/existent/path",
+        "useless body",
       );
 
-      expect(status).not.toBeNull();
-      expect(status).toBeDefined();
-      expect(message).not.toBeNull();
-      expect(message).toBeDefined();
-
-      if (status === "ok") {
-        try {
-          const body = await fsp.readFile(dest);
-          expect(body).toBe("Test contents");
-          fs.rmSync(dest);
-        } catch (_) {}
-      }
+      expect(tag).toBe("error");
     });
   });
 
@@ -41,44 +41,42 @@ describe("repositories", () => {
 
       const res = await repositories.readDeck(src);
 
-      expect(res.status).toBe("ok");
-      expect(res.data).toBe(data);
+      expect(res.tag).toBe("ok");
+      expect(res.value).toBe(data);
     });
 
     it("should return an error if the deck does not exist", async () => {
       const res = await repositories.readDeck("./non/existent/path");
 
-      expect(res.status).toBe("error");
+      expect(res.tag).toBe("error");
     });
   });
 
   describe("backupDeck", () => {
     it("should make an exact copy of the deck", async () => {
-      const { status, message } = await repositories.backupDeck(src, dest);
+      const { tag } = await repositories.backupDeck(src, dest);
 
-      expect(status).toBe("ok");
+      expect(tag).toBe("ok");
 
       const cpContents = await fsp.readFile(dest, { encoding: "utf8" });
       expect(cpContents).toBe("Test contents");
+
       await fsp.rm(dest);
     });
 
     it("should return an error if the source does not exist", async () => {
-      const { status } = await repositories.backupDeck(
+      const { tag } = await repositories.backupDeck(
         "./non/existent/path",
         dest,
       );
 
-      expect(status).toBe("error");
+      expect(tag).toBe("error");
     });
 
     it("should return an error if the destination does not exist", async () => {
-      const { status } = await repositories.backupDeck(
-        src,
-        "./non/existent/path",
-      );
+      const { tag } = await repositories.backupDeck(src, "./non/existent/path");
 
-      expect(status).toBe("error");
+      expect(tag).toBe("error");
     });
   });
 });
