65a3c665816056ef3d460286b355c49031423696
Eloy Robillard <eloy.robillard@gmail.com>
2025-08-25 15:58:33 +0900

    Add comment to CSV checker

--- apps/server/src/checkers.js
+++ apps/server/src/checkers.js
@@ -7,6 +7,7 @@ export const isValidFormat = (input) => {
   const byNewline = input.split("\n");
   const byComma = byNewline.map((subs) => subs.split(/","/));
 
+  // byCommaの最後の要素は空の文字列になっている
   for (let i = 0; i < byComma.length - 1; i++) {
     if (byComma[i].length != 2) {
       return { succeeded: false, errorOn: byComma[i] };
